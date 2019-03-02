2c451c4025a8d484f86cb049c1a6575d6e37e178
Dorisa <doris_shehi2@hotmail.com>
2019-03-02 17:46:51 +0100

    ext_emconf changed

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.6',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.99 - 8.7.99',
+			'typo3' => '8.7.0 - 8.7.99',
 		),
 		'conflicts' => array(
 		),
